96db92e51238b4bfaf72f773c2c7fafd8a35f530
Michael Stuart <mike.w.stuart@gmail.com>
2017-08-24 16:55:53 -0700

    trying things

--- src/routes/photo.js
+++ src/routes/photo.js
@@ -10,32 +10,33 @@ import { formatImageTags } from '../helpers';
 const upload = multer({ dest: 'uploads/' });
 const router = Router();
 
-// let visionClient;
-//
-// jsonfile.writeFile('keyFile.json', {
-//   "type": process.env.G_TYPE,
-//   "project_id": process.env.G_PROJECT_ID,
-//   "private_key_id": process.env.G_PRIVATE_KEY_ID,
-//   "private_key": process.env.G_PRIVATE_KEY,
-//   "client_email": process.env.G_CLIENT_EMAIL,
-//   "client_id": process.env.G_CLIENT_ID,
-//   "auth_uri": process.env.G_AUTH_URI,
-//   "token_uri": process.env.G_TOKEN_URI,
-//   "auth_provider_x509_cert_url": process.env.G_PROVIDER_CERT_URL,
-//   "client_x509_cert_url": process.env.G_CLIENT_CERT_URL,
-// }, () => {
-//   visionClient = Vision({
-//     projectId: process.env.G_PROJECT_ID,
-//     keyFilename: 'keyFile.json',
-//   });
-// });
+let visionClient;
 
-const visionClient = Vision({
-  key: process.env.API_KEY,
-  projectId: process.env.G_PROJECT_ID,
+jsonfile.writeFile('keyFile.json', {
+  "type": process.env.G_TYPE,
+  "project_id": process.env.G_PROJECT_ID,
+  "private_key_id": process.env.G_PRIVATE_KEY_ID,
+  "private_key": process.env.G_PRIVATE_KEY,
+  "client_email": process.env.G_CLIENT_EMAIL,
+  "client_id": process.env.G_CLIENT_ID,
+  "auth_uri": process.env.G_AUTH_URI,
+  "token_uri": process.env.G_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.G_PROVIDER_CERT_URL,
+  "client_x509_cert_url": process.env.G_CLIENT_CERT_URL,
+}, () => {
+  visionClient = Vision({
+    keyFilename: 'keyFile.json',
+    key: process.env.API_KEY,
+    projectId: process.env.G_PROJECT_ID,
+  });
+  console.log(visionClient)
 });
 
-console.log(visionClient)
+// const visionClient = Vision({
+//   key: process.env.API_KEY,
+//   projectId: process.env.G_PROJECT_ID,
+// });
+
 
 router.post('/', upload.single('photo'), (req, res, next) =>
   visionClient.labelDetection({ source: { filename: req.file.path } })

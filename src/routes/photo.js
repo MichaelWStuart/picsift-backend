02bbac6806d8b712b7c146ba21e847deaa17c783
Michael Stuart <mike.w.stuart@gmail.com>
2017-08-25 16:22:22 -0700

    doing dangerous test

--- src/routes/photo.js
+++ src/routes/photo.js
@@ -10,24 +10,34 @@ import { formatImageTags } from '../helpers';
 const upload = multer({ dest: 'uploads/' });
 const router = Router();
 
-let visionClient;
+// let visionClient;
 
-jsonfile.writeFile('keyFile.json', {
-  "type": process.env.G_TYPE,
-  "project_id": process.env.G_PROJECT_ID,
-  "private_key_id": process.env.G_PRIVATE_KEY_ID,
-  "private_key": process.env.G_PRIVATE_KEY,
-  "client_email": process.env.G_CLIENT_EMAIL,
-  "client_id": process.env.G_CLIENT_ID,
-  "auth_uri": process.env.G_AUTH_URI,
-  "token_uri": process.env.G_TOKEN_URI,
-  "auth_provider_x509_cert_url": process.env.G_PROVIDER_CERT_URL,
-  "client_x509_cert_url": process.env.G_CLIENT_CERT_URL,
-}, () => {
-  visionClient = Vision({
-    keyFilename: 'keyFile.json',
-  });
+const visionClient = Vision({
+  keyFilename: 'picsift-b92bb86b9d8d.json',
 });
+
+// console.log(visionClient)
+
+
+
+// jsonfile.writeFile('keyFile.json', {
+//   "type": process.env.G_TYPE,
+//   "project_id": process.env.G_PROJECT_ID,
+//   "private_key_id": process.env.G_PRIVATE_KEY_ID,
+//   "private_key": process.env.G_PRIVATE_KEY,
+//   "client_email": process.env.G_CLIENT_EMAIL,
+//   "client_id": process.env.G_CLIENT_ID,
+//   "auth_uri": process.env.G_AUTH_URI,
+//   "token_uri": process.env.G_TOKEN_URI,
+//   "auth_provider_x509_cert_url": process.env.G_PROVIDER_CERT_URL,
+//   "client_x509_cert_url": process.env.G_CLIENT_CERT_URL,
+// }, () => {
+  // visionClient = Vision({
+  //   keyFilename: 'keyFile.json',
+  // });
+//   console.log(visionClient)
+// });
+
 
 router.post('/', upload.single('photo'), (req, res, next) =>
   visionClient.labelDetection({ source: { filename: req.file.path } })

6355fc6945403741bc0e58bd6152d4ba2da85deb
Michael Stuart <mike.w.stuart@gmail.com>
2017-08-28 13:12:56 -0700

    this is gonna work

--- src/routes/photo.js
+++ src/routes/photo.js
@@ -10,37 +10,28 @@ import { formatImageTags } from '../helpers';
 const upload = multer({ dest: 'uploads/' });
 const router = Router();
 
-// let visionClient;
+let visionClient;
 
-const visionClient = Vision({
-  keyFilename: 'picsift-b92bb86b9d8d.json',
+jsonfile.writeFile('picsift-77ef34dda90a.json', {
+  "type": process.env.G_TYPE,
+  "project_id": process.env.G_PROJECT_ID,
+  "private_key_id": process.env.G_PRIVATE_KEY_ID,
+  "private_key": process.env.G_PRIVATE_KEY,
+  "client_email": process.env.G_CLIENT_EMAIL,
+  "client_id": process.env.G_CLIENT_ID,
+  "auth_uri": process.env.G_AUTH_URI,
+  "token_uri": process.env.G_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.G_PROVIDER_CERT_URL,
+  "client_x509_cert_url": process.env.G_CLIENT_CERT_URL,
+}, () => {
+  visionClient = Vision({
+    keyFilename: 'picsift-77ef34dda90a.json',
+  });
 });
-
-// console.log(visionClient)
-
-
-
-// jsonfile.writeFile('keyFile.json', {
-//   "type": process.env.G_TYPE,
-//   "project_id": process.env.G_PROJECT_ID,
-//   "private_key_id": process.env.G_PRIVATE_KEY_ID,
-//   "private_key": process.env.G_PRIVATE_KEY,
-//   "client_email": process.env.G_CLIENT_EMAIL,
-//   "client_id": process.env.G_CLIENT_ID,
-//   "auth_uri": process.env.G_AUTH_URI,
-//   "token_uri": process.env.G_TOKEN_URI,
-//   "auth_provider_x509_cert_url": process.env.G_PROVIDER_CERT_URL,
-//   "client_x509_cert_url": process.env.G_CLIENT_CERT_URL,
-// }, () => {
-  // visionClient = Vision({
-  //   keyFilename: 'keyFile.json',
-  // });
-//   console.log(visionClient)
-// });
-
 
 router.post('/', upload.single(), (req, res, next) => {
   console.log('FILE FILE FILE', req.file)
+  console.log(visionClient)
   visionClient.labelDetection({ source: { filename: req.file.path } })
     .then(results => {
       Photo.create({
